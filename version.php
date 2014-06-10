54462275d76b48efbae3251838b42f09414f0aa2
Nicolas Dunand <nico@kolya.ch>
2014-06-10 14:34:45 +0200

    Added plugin maturity and release information.

--- version.php
+++ version.php
@@ -28,3 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2014050200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014050800;        // Requires this Moodle version.
 $plugin->component = 'atto_corrections';  // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '1.0 RC';
+
