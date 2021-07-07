8aca38e5d97ee127c10516b26e51d0b581132041
Nicolas Dunand <nico@kolya.ch>
2021-07-07 15:53:59 +0200

    Bumped version number for new release

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021070604;
+$plugin->version   = 2021070700;
 $plugin->requires  = 2014050800;
 $plugin->component = 'atto_corrections';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.8 for Moodle 2.7-3.5 (Build 2018050201)';
+$plugin->release = '1.0.8 for Moodle 2.7-3.11 (Build 2021070700)';
 
